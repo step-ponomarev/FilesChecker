6d9f745534802a4ac5e0be390a3ae1c9b9f72089
Ponomarev Stepan <htconesuper11@gmail.com>
2019-05-01 03:15:04 +0300

    Script created

---
+++ main.py
@@ -0,0 +1,28 @@
+import os, sys
+
+direct = os.getcwd() + '/'
+
+def bust_script(direct):
+  
+  if (os.listdir(direct)):
+    for obj in os.listdir(direct):
+      strings = int(0)
+
+      if (os.path.isdir(direct + '/' + obj) == True):
+        newdir = direct + '/' + obj + '/'
+        bust_script(newdir)
+      elif (os.path.isfile(direct + obj) == True):
+        try:
+          with open(obj, 'r', encoding = 'utf-8') as file:
+            for line in file:
+              strings += 1
+        except:
+          pass
+
+        print('Имя:', obj, '\nКоличество строк:', strings)
+        print('-------------------')
+  else:
+    print('Директория:',direct,'\nСтатус: Пуста')
+    print('-------------------')
+
+bust_script(direct)
